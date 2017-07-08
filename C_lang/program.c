eb87acaf94ac2ce14f9dd31adf13bf6025c85004
Gavin Hell <gmangavin1@gmail.com>
2017-07-08 14:31:41 -0700

    v1.2.0
    program:
    First 29 line program is here :)

--- C_lang/program.c
+++ C_lang/program.c
@@ -1,7 +1,29 @@
-#include <stdio.h>
+/* 
+You can view my notes on other languages (C# / Python) below
+https://wwww.bit.ly/JeeCsharp or https://wwww.bit.ly/JeePython
+*/ 
 
-void main()
+#include <stdio.h> //imports library stdio
+
+int globalvar = 100;
+
+main()
 {
-	printf("Hello from C using Visual Studio!\n");
-	getchar();
+	char localvar = 'A';
+	char StringVar[] = "Hello again, this is a test of string";
+
+	printf("Hello friend, welcome to this C program test");
+	printf("The globalVar is: %d \n", globalvar);
+	printf("The localvar is: %c \n", localvar);
+	printf("The stringVar is: %s \n", StringVar);
+
+	char UserChar;
+	printf("\n\nWhat is your favorite letter, my friend: ");
+	scanf("%c", &UserChar);
+	printf("Very good, interesting that you chose: %c \n", UserChar);
+
+	char UserString[30];
+	printf("Now give me more characters my friend: \n");
+	scanf(" %s", UserString);
+	printf("I see my friend, you typed: %s \n", UserString);
 }
